a90515d012ed079f25cc6bf14f65dcf6c4dfe99a
Oleg9791 <oleg9791@tut.by>
2020-12-05 16:02:06 +0300

    calc

--- PHP/Index.php
+++ PHP/Index.php
@@ -1,11 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-   <?php echo 'Hello Word' ?> 
+    <!-- <?php echo 5 * 3 ?> -->
+    <?php echo 2 ** 5 * (5 - 4) + 89 ** 5 ?>
+
+
 </body>
+
 </html>
