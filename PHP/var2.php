94878889654f6dbf913fd11ebe91626e83b0dd38
Oleg9791 <oleg9791@tut.by>
2020-12-08 21:21:11 +0300

    php var

---
+++ PHP/var2.php
@@ -0,0 +1,13 @@
+<?php
+$a = 2;
+$b = 3;
+echo $a + $b;
+?>
+<br>
+<?php
+
+$a = ($b = 4) + 5;
+echo $a;
+echo "<br>\n";
+echo $b;
+?>
---
+++ PHP/var5.php
@@ -0,0 +1,22 @@
+<?php
+$x = 5;
+$y = $x * $x;
+echo $y;
+?>
+<?php
+$x = 4;
+$y = sqrt($x) / $x ** 2;
+echo $y;
+?>
+<?php
+$a = 4;
+$b = 3;
+$y = abs($a - $b) / sin($b) * sqrt($b + $a);
+echo $y;
+?>
+<?php
+$a = 2;
+$b = 8;
+$c = 6;
+$x = - ($b + sqrt($b ** 2 - 4 * $a * $c)) / 2 * $a;
+echo $x;
